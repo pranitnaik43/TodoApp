3c3ad58a42b3e32294184846f2474c618ff4be1f
pranit naik <pranitnaik43@gmail.com>
2022-01-08 21:23:14 +0530

    minor change

--- server/index.js
+++ server/index.js
@@ -25,10 +25,10 @@ const PORT = (process.env.PORT) ? (process.env.PORT) : 3001;
 
     if (process.env.NODE_ENV === 'production') {
       // Serve any static files
-      app.use(express.static(path.join(__dirname, 'client/build')));
+      app.use(express.static(path.join(__dirname, '../client/build')));
       // Handle React routing, return all requests to React app
       app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+        res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
       });
     }
 
