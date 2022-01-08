8b3364cfff93ab9ed172f35751a1ce9f5997e3b0
pranit naik <pranitnaik43@gmail.com>
2022-01-08 21:05:48 +0530

    name changed

--- server/index.js
+++ server/index.js
@@ -24,10 +24,10 @@ const PORT = (process.env.PORT) ? (process.env.PORT) : 3001;
 
     if (process.env.NODE_ENV === 'production') {
       // Serve any static files
-      app.use(express.static(path.join(__dirname, 'todo-frontend/build')));
+      app.use(express.static(path.join(__dirname, 'client/build')));
       // Handle React routing, return all requests to React app
       app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, 'todo-frontend/build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
       });
     }
 
